e51830be482ffc10588bf3e9b7471d2d1c0c2d37
FNZ <hakimfenzi10@gmail.com>
2025-04-23 10:10:49 +0200

    Fin de Tp coverage

--- src/main/java/org/example/TP2/jeu/interfaces/Banque.java
+++
@@ -1,7 +0,0 @@
-package org.example.TP2.jeu.interfaces;
-
-public interface Banque {
-    void crediter(int somme);
-    boolean est_solvable();
-    void debiter(int somme);
-}
--- src/main/java/org/example/TP2/jeu/interfaces/DebitImpossibleException.java
+++
@@ -1,7 +0,0 @@
-package org.example.TP2.jeu.interfaces;
-
-public class DebitImpossibleException extends Exception {
-    public DebitImpossibleException() {
-        super("Le joueur n'a pas assez d'argent");
-    }
-}
